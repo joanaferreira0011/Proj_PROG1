3944272ac4413879794e6befb408b7a830e5e31f
Joana Ferreira <joanaferreira0011@gmail.com>
2018-03-30 13:23:11 +0100

    Open the file and check if it exists

--- main.cpp
+++ main.cpp
@@ -1,9 +1,25 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 #include <vector>
+#include <iomanip>
 
 using namespace std;
 
 int main() {
-    cout << "Hello, World!" << endl;
-    return 0;
-}
+    string fileName;
+    ifstream f;
+    string line;
+
+    // Read the file name
+    cout << "File name ? ";
+    cin >> fileName;
+
+    // Open the file; exit program if the file couldn't be opened
+    f.open(fileName);
+    if (!f.is_open())
+        { cerr << "File " << fileName << " not found !\n";
+        return(1); }
+
+
+    return 0;}
