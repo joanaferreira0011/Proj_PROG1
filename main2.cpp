4355942cb194b35aa386f53fbb0a85d580f25baa
João Nunes <joaopaulo_n@hotmail.com>
2018-04-13 21:39:28 +0100

    Warning solved

--- main2.cpp
+++ main2.cpp
@@ -135,6 +135,55 @@ bool invalid_input(string word, string word_given)
 	return false;
 }
 
+bool wildcardMatch(const char *str, const char *strWild)
+{
+	// We have a special case where string is empty ("") and the mask is "*".
+	// We need to handle this too. So we can't test on !*str here.
+	// The loop breaks when the match string is exhausted.
+	while (*strWild)
+	{
+		// Single wildcard character
+		if (*strWild == '?')
+		{
+			// Matches any character except empty string
+			if (!*str)
+				return false;
+			// OK next
+			++str;
+			++strWild;
+		}
+		else if (*strWild == '*')
+		{
+			// Need to do some tricks.
+			// 1. The wildcard * is ignored.
+			// So just an empty string matches. This is done by recursion.
+			// Because we eat one character from the match string,
+			// the recursion will stop.
+			if (wildcardMatch(str, strWild + 1))
+				// we have a match and the * replaces no other character
+				return true;
+			// 2. Chance we eat the next character and try it again,
+			// with a wildcard * match. This is done by recursion.
+			// Because we eat one character from the string,
+			// the recursion will stop.
+			if (*str && wildcardMatch(str + 1, strWild))
+				return true;
+			// Nothing worked with this wildcard.
+			return false;
+		}
+		else
+		{
+			// Standard compare of 2 chars. Note that *str might be 0 here,
+			// but then we never get a match on *strWild
+			// that has always a value while inside this loop.
+			if (toupper(*str++) != toupper(*strWild++))
+				return false;
+		}
+	}
+	// Have a match? Only if both are at the end...
+	return !*str && !*strWild;
+}
+
 // --------- Options --------- //
 
 void find_word(vector <string> v1)
@@ -294,55 +343,6 @@ void randomletters(vector <string> v1)
 
 } //Option4
 
-bool wildcardMatch(const char *str, const char *strWild)
-{
-	// We have a special case where string is empty ("") and the mask is "*".
-	// We need to handle this too. So we can't test on !*str here.
-	// The loop breaks when the match string is exhausted.
-	while (*strWild)
-	{
-		// Single wildcard character
-		if (*strWild== '?')
-		{
-			// Matches any character except empty string
-			if (!*str)
-				return false;
-			// OK next
-			++str;
-			++strWild;
-		}
-		else if (*strWild== '*')
-		{
-			// Need to do some tricks.
-			// 1. The wildcard * is ignored.
-			// So just an empty string matches. This is done by recursion.
-			// Because we eat one character from the match string,
-			// the recursion will stop.
-			if (wildcardMatch(str,strWild+1))
-				// we have a match and the * replaces no other character
-				return true;
-			// 2. Chance we eat the next character and try it again,
-			// with a wildcard * match. This is done by recursion.
-			// Because we eat one character from the string,
-			// the recursion will stop.
-			if (*str && wildcardMatch(str+1,strWild))
-				return true;
-			// Nothing worked with this wildcard.
-			return false;
-		}
-		else
-		{
-			// Standard compare of 2 chars. Note that *str might be 0 here,
-			// but then we never get a match on *strWild
-			// that has always a value while inside this loop.
-			if (toupper(*str++)!=toupper(*strWild++))
-				return false;
-		}
-	}
-	// Have a match? Only if both are at the end...
-	return !*str && !*strWild;
-}
-
 void option5(vector<string> words, int size)
 { string wild;
 		vector<string> possible_words;
@@ -359,13 +359,13 @@ void option5(vector<string> words, int size)
 		if (possible_words.size()==0)
 			cout << "There are no matches.\n";
 		else
-		{   int a=0;
+		{   size_t a=0;
 			cout<< "Match with: "<<endl;
 			while(a< possible_words.size())
 			{cout<<possible_words.at(a)<< endl;
 				a++;}
 			
-		}}
+		}} //Option5
 	
 // ---------------  MENU -------------------------- //
 
