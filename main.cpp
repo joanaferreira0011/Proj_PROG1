dcac4ad99130ad0027c32388a4b7cd3d13ffd46a
João Nunes <joaopaulo_n@hotmail.com>
2018-04-13 16:08:32 +0100

    Errors fixed, add option 2,3 and 4, add simplified and add extra functions for better operation

--- main.cpp
+++ main.cpp
@@ -9,158 +9,159 @@
 using namespace std;
 
 // --- Algorithm that orders the vector --- //
-void sort(vector<string> &v1) 
-{
+void sort(vector<string> &v1) {
 	// Sort elements in their range (start to finish)
-	sort(v1.begin(), v1.end());  
+	sort(v1.begin(), v1.end());
 }
 
 
 // --- Algorithm that removes duplicate words --- //
-void remove_duplicates(vector<string> &v1)
-{
+void remove_duplicates(vector<string> &v1) {
 	// Considering that the vector is already in order
 	v1.erase(unique(v1.begin(), v1.end()), v1.end()); // Remove duplicate words in their range (start to finish)
 }
 
 //separate words:
 // receives lines with ';' from vector_line
-void separate_words(string line, vector<string> &word_list_v)
+void separate_words(const string &line, vector<string> &word_list_v) {
+	unsigned int i = 0;
+	unsigned int first = 0;
+	while (i < line.length()) {
+		if (i == (line.length() - 1)) {
+			word_list_v.push_back(line.substr(first, i - first + 1));
+			i++;
+		} else {
+			if (line.at(i) == ';' || line.at(i) == ' ') {
+				word_list_v.push_back(line.substr(first, i - first));
+				first = i + 2;
+				i = i + 2;
+			} else
+				i++;
+		}
+	}
+}
+
+
+bool has_headlines(const string &line)  //check if the line has headlines, called by vector_line
 {
-    int i=0;
-    int first=0;
-    while (i<line.length())
-    {if (i==(line.length()-1))
-        {word_list_v.push_back(line.substr(first, i-first+1));
-        i++;}
-        else
-        {if (line.at(i)== ';' || line.at(i)== ' ')
-            {word_list_v.push_back(line.substr(first, i-first));
-            first= i+2;
-            i=i+2;}
-        else
-                i++;}}
+	unsigned int i = 0;
+	if (!line.empty()) {
+		while (i < line.length()) {
+			if (isupper(line.at(i)) || line.at(i) == ';' || line.at(i) == ' ')
+				i++;
+			else break;
+		}
+	} else
+		return false;
+	
+	if (i == line.length())
+		return true;
 }
 
-
-bool has_headlines (string line)  //check if the line has headlines, called by vector_line
-{int i=0;
-    if (!line.empty())
-       {while (i< line.length())
-            { if (isupper(line.at(i)) || line.at(i)== ';' || line.at(i)== ' ')
-                    i++;
-                else break; }}
-    else
-        return false;
-
-    if (i==line.length())
-        return true;
-}
-
-bool check_if_up_space(string line) //check if all ar uppercase or space
+bool check_if_up_space(const string &line) //check if all ar uppercase or space
 {
-    int i=0;
-    while (i< line.length())
-     {if (isupper(line.at(i)) || line.at(i)== ' ' )
-        i++;
-        else
-            break;}
+	unsigned int i = 0;
+	while (i < line.length()) {
+		if (isupper(line.at(i)) || line.at(i) == ' ')
+			i++;
+		else
+		    return false;
+	}
+	return true;
 
-    if (i==line.length())
-        return true;
-    else return false;
-
+	
 }
 
 
-void vector_line(string line, vector<string> &word_list_v)  //returns a vector with words from line
-{   if (has_headlines(line) && (line.find(' ') == string::npos))
-        word_list_v.push_back(line);
-
-    if(has_headlines(line) && (!(check_if_up_space(line)) &&(line.find('\'') == string::npos) && (line.find(';') != string::npos)))
-       /* {if (!(check_if_up_space(line))  //check if all ar uppercase or space and discard if true
-         { if (line.find(';') != string::npos)
-                /* word_list_v.push_back(line);
-            else  */
-                separate_words(line, word_list_v);
+void vector_line(const string &line, vector<string> &word_list_v)  //returns a vector with words from line
+{
+	if (has_headlines(line) && (line.find(' ') == string::npos))
+		word_list_v.push_back(line);
+	
+	if (has_headlines(line) &&
+	    (!(check_if_up_space(line)) && (line.find('\'') == string::npos) && (line.find(';') != string::npos)))
+		/* {if (!(check_if_up_space(line))  //check if all ar uppercase or space and discard if true
+		  { if (line.find(';') != string::npos)
+				 /* word_list_v.push_back(line);
+			 else  */
+		separate_words(line, word_list_v);
 }
-
 
 
 //extract from dictionary
-void extract(string dictionary_file_name, vector<string> &word_list_v)
-    {
-    string line;
-
-    ifstream dictionary(dictionary_file_name);
-
-    // Open the file; exit program if the file couldn't be opened
-   // dictionary.open(dictionary_file_name);
-    if (!dictionary.is_open())
-        { cerr << "File " << "dictionary" << " not found !\n"; }
-
-    while (!dictionary.eof())
-        {   getline(dictionary, line);
-            vector_line(line, word_list_v);
-        }
-
-    dictionary.close();
-    }
-
-void write_to_file(string word_list_file_name, vector<string> vector, int max )
-{
-	ofstream file(word_list_file_name);
-	int i= 1;
+void extract(const string &dictionary_file_name, vector<string> &word_list_v) {
+	string line;
 	
-	while(i< max)
-	{file<<vector[i]<< endl;
-	i++;}
+	ifstream dictionary(dictionary_file_name);
+	
+	// Open the file; exit program if the file couldn't be opened
+	// dictionary.open(dictionary_file_name);
+	if (!dictionary.is_open()) { cerr << "File " << "dictionary" << " not found !\n"; exit(1); }
+	
+	while (!dictionary.eof()) {
+		getline(dictionary, line);
+		vector_line(line, word_list_v);
+	}
+	
+	dictionary.close();
+}
+
+void write_to_file(const string &word_list_file_name, vector<string> vector, int max) {
+	ofstream file(word_list_file_name);
+	int i = 1;
+	
+	while (i < max) {
+		file << vector[i] << endl;
+		i++;
+	}
 	
 	file.close();
-
+	
 }
+
 int main() {
-    string dictionary_file_name;
-    string word_list_file_name;
-    string line;
-    vector<string> word_list_v;
-
-    //INTERFACE HEADLINE
-    cout << "EXTRACTION OF WORD LIST FROM DICTIONARY" << endl <<"======================================="<< endl;
-
-    // Read the file name //
-    cout << "Dictionary file ? ";
-    cin >> dictionary_file_name;
+	string dictionary_file_name;
+	string word_list_file_name;
+	string line;
+	vector<string> word_list_v;
+	
+	//INTERFACE HEADLINE
+	cout << "EXTRACTION OF WORD LIST FROM DICTIONARY" << endl << "=======================================" << endl;
+	
+	// Read the file name //
+	cout << "Dictionary file ? ";
+	cin >> dictionary_file_name;
 	cout << "Word list file ? ";
 	cin >> word_list_file_name;
-
-    extract(dictionary_file_name,word_list_v);
-    //int number_of_words= word_list_v.size();
-    //cout<< number_of_words;
-    // Read the word_list file name //
-
-
-
-    cout << "Extracting simple words from file" << dictionary_file_name << endl << "," << "begining with letter...";
-
-
-	cout << "Number of simple words" << " = " << word_list_v.size() << endl;
+	
+	extract(dictionary_file_name, word_list_v);
+	//int number_of_words= word_list_v.size();
+	//cout<< number_of_words;
+	// Read the word_list file name //
+	
+	
+	
+	cout << "Extracting simple words from file" << dictionary_file_name << endl << "," << "begining with letter...";
+	
+	
+	cout << "Number of simple words" << " = " << word_list_v.size() - 1 << endl;
 	cout << "Sorting words " << "..." << endl;
 	
 	//Sort vector
-	sort(word_list_v); 
-
+	sort(word_list_v);
+	
 	cout << "Removing duplicate words " << "..." << endl;
-
+	
 	// Delete repeated vector words
 	remove_duplicates(word_list_v);
-
-	cout << "Number of non-duplicate simple words" << " = " << word_list_v.size() << endl;
-
-	cout << "Saving words into file " << word_list_file_name << " ..." << endl;
-	write_to_file(word_list_file_name, word_list_v, word_list_v.size() );
 	
-
+	cout << "Number of non-duplicate simple words" << " = " << word_list_v.size() - 1 << endl;
+	
+	cout << "Saving words into file " << word_list_file_name << " ..." << endl;
+	write_to_file(word_list_file_name, word_list_v, word_list_v.size());
+	
+	
 	cout << "End of processing." << endl;
- 
-	return 0;}
+	
+	return 0;
+}
--- main2.cpp
+++ main2.cpp
@@ -7,30 +7,79 @@
 #include <iomanip>
 #include <cstddef>
 #include <algorithm>
+#include <time.h>
 
 using namespace std;
 
+// -------------- Function prototypes -------------- //
 
+vector<string> getwords(string file_name);
+void uppercase_letters(string &word);
+void frequency(vector<string> v1, vector<int> &frequencyvector, size_t &bigword);
+int lowerfrequency(vector<int> frequencyvector);
+bool invalid_input(string word, string word_given);
+void find_word(vector <string> v1);
+void word_guess(vector <string> v1);
+void searchwords(vector <string> v1);
+void randomletters(vector <string> v1);
+void menu(vector <string> vectorwords);
+void usercontinue(vector <string> v1);
+
+// ------------------------------------------------- //
+
+int main()
+{
+	cout << " ............ " << endl;
+	cout << " ..  MENU  .. " << endl;
+	cout << " ............ " << endl;
+	
+	cout << "Enter the name of the file, which contains the list of words: ";
+	string file_name;
+	getline(cin, file_name);
+	
+	vector<string> vectorwords = getwords(file_name);
+	
+	cout << "Choose one of the following options: " << endl; // Shows on the screen the available options
+	cout << "Option 1: Verify that the word belongs to the word list " << endl;
+	cout << "Option 2: Guess a randomly chosen word from the list " << endl;
+	cout << "Option 3: Show all words with a certain set of letters given by the user " << endl;
+	cout << "Option 4: A set of letters are given, for the user to construct a word and check if it belongs to the list " << endl;
+	cout << "Option 5: Ask the user for a string with wildcard characters, and shows all words matching the string requested " << endl;
+	cout << "Option 6: Exit the program" << endl;
+
+	menu(vectorwords);
+
+	usercontinue(vectorwords); // For if the user wants to continue
+
+	return 0;}
+
+// -------------- Input File Name ------------------ //
 
 vector<string> getwords(string file_name)  //input a file name, opens file a returns a vector with words from file
 {
-string line;
-ifstream dictionary(file_name);
-vector<string> words;
+	string line;
+	ifstream dictionary(file_name);
+	vector<string> words;
 
-// Open the file; exit program if the file couldn't be opened
-// dictionary.open(dictionary_file_name);
-if (!dictionary.is_open())
-{ cerr << "File " << "dictionary" << " not found !\n"; }
+	// Open the file; exit program if the file couldn't be opened
+	// dictionary.open(dictionary_file_name);
+	if (!dictionary.is_open())
+	{
+		cerr << "File " << "dictionary" << " not found !\n";
+		exit(1);
+	}
 
-while (!dictionary.eof())
-{   getline(dictionary, line);
- words.push_back(line);
+	while (!dictionary.eof())
+	{
+		getline(dictionary, line);
+		words.push_back(line);
+	}
+
+	return words;
 }
 
-return words;}
 
-
+// --------- Functions used in options --------- //
 
 void uppercase_letters(string &word)
 {
@@ -39,8 +88,57 @@ void uppercase_letters(string &word)
 	transform(word.begin(), word.end(), word.begin(), toupper);
 }
 
+void frequency(vector<string> v1, vector<int> &frequencyvector, size_t &bigword)
+{
+	//Vector with the frequency of each letter
+	for (size_t i = 0; i < v1.size(); i++)
+	{
+		//Size of the biggest word
+		if (bigword < v1[i].length())
+			bigword = v1[i].length();
+
+		for (size_t j = 0; j < v1[i].length(); j++)
+		{
+			frequencyvector[v1[i][j] - 65] = frequencyvector[v1[i][j] - 65]++;
+		}
+	}
+}
+
+int lowerfrequency(vector<int> frequencyvector)
+{
+	// Letters with lower frequency	
+	int min = frequencyvector[0];
+
+	for (size_t i = 1; i < frequencyvector.size(); i++)
+	{
+		if (min > frequencyvector[i])
+			min = frequencyvector[i];
+	}
+
+	return min;
+}
+
+bool invalid_input(string word, string word_given)
+{
+	// Make sure the user uses only the given letters or a subset of them
+
+	for (size_t i = 0; i < word.size(); i++)
+	{
+		if (word_given.find_first_of(word[i]) != -1)
+			word_given.erase(word_given.find_first_of(word[i]), 1);
+		else
+			return true;
+	}
+
+	return false;
+}
+
+// --------- Options --------- //
+
 void find_word(vector <string> v1)
 {
+	cout << "Enter a word, to see if it is in the dictionary ";
+
 	string insert_word;
 	cin >> insert_word;
 
@@ -59,7 +157,7 @@ void find_word(vector <string> v1)
 void word_guess(vector <string> v1)
 {
 	// Choose a random word and show
-	srand(time(0));
+	srand((int)time(NULL));
 	int indicewordrandom = rand() % v1.size();
 	string wordrandom = v1.at(indicewordrandom);
 
@@ -101,44 +199,168 @@ void word_guess(vector <string> v1)
 	}
 } //Option2
 
-
-int main()
+void searchwords(vector <string> v1)
 {
+	// Displays on the screen the words that are possible with the letters requested
+
+	// user inserts the letter set
+	string stringletters;
+	cout << "Enter a set of letters to identify the words: ";
+	cin >> stringletters;
+
+	uppercase_letters(stringletters);
+
+	vector <string> validwords; // vector containing words that can be formed
+
+	sort(stringletters.begin(), stringletters.end()); // Sort string
+
+	do
+	{
+		if (binary_search(v1.begin(), v1.end(), stringletters))  // Search Words 
+			validwords.push_back(stringletters); // Put in vector
+
+	} while (next_permutation(stringletters.begin(), stringletters.end())); // Make permutations between letters
+
+																			// Show Words
+
+	if (validwords.size() == 0)
+		cout << "There are no words in the dictionary with this set of letters" << endl;
+	else
+	{
+		cout << "Words from the dictionary that are formed with the indicated letters" << endl;
+
+		for (size_t i = 0; i < validwords.size(); i++)
+			cout << validwords.at(i) << endl;
+	}
+} //Option3
+
+void randomletters(vector <string> v1)
+{
+	vector<int> frequencyvector(26); //Vector with the frequency of each letter
+	vector<char> larger_set;
+	int min, n;
+	size_t	bigword = 0;
+	char letter = 'A';
+	string word, word_given;
+
+	frequency(v1, frequencyvector, bigword);
+
+	min = lowerfrequency(frequencyvector);
+
+	for (size_t i = 0; i < frequencyvector.size(); i++)
+	{
+		int y = frequencyvector[i] * 2 / min;
+
+		for (int d = 0; d <= y; d++)
+			larger_set.push_back(char(letter + i));
+	}
+
+	srand((int)time(NULL));
+
+	n = rand() % (bigword - 1); //Number of letter the word is going to have
+
+	for (int i = 0; i <= n; i++)
+	{
+		int j = rand() % (larger_set.size() - 1);
+		word_given = word_given + larger_set[j];
+		larger_set.erase(larger_set.begin() + j);
+	}
+
+	cout << word_given;
+
+	cout << endl << "Word: ";
+
+	cin >> word;
+
+	uppercase_letters(word);
+
+	while (invalid_input(word, word_given))
+	{
+		cin.clear();
+		cin.ignore(10000, '\n');
+		cout << "Not a valid word!" << endl;
+		cout << "Word: ";
+		cin >> word;
+		uppercase_letters(word);
+	}
+
+
+	if (find(v1.begin(), v1.end(), word) != v1.end())
+		cout << "Word found!" << endl;
+	else
+		cout << "Word not found!" << endl;
+
+} //Option4
+
+// ---------------  MENU -------------------------- //
+
+void menu(vector <string> vectorwords) // The user enters an option
+{
+
 	int option;
-
-	ifstream arq("procdic");
-
-
-	cout << "Choose one of the following options: " << endl; // Shows on the screen the available options
-	cout << "Option 1: Verify that the word belongs to the word list " << endl;
-	cout << "Option 2: Guess a randomly chosen word from the list " << endl;
-	cout << "Option 3: Show all words with a certain set of letters given by the user " << endl;
-	cout << "Option 4: A set of letters are given, for the user to construct a word and check if it belongs to the list " << endl;
-	cout << "Option 5: Ask the user for a string with wildcard characters, and shows all words matching the string requested " << endl;
-
+	cout << "Enter option ";
 	cin >> option; // The user enters an option
 
-		switch (option) //incomplete
+	switch (option)
+	{
+	case 1:
+		cout << " ---- OPTION 1 ---- " << endl;
+		find_word(vectorwords);
+		break;
+	case 2:
+		cout << " ---- OPTION 2 ---- " << endl;
+		word_guess(vectorwords);
+		break;
+	case 3:
+		cout << " ---- OPTION 3 ---- " << endl;
+		searchwords(vectorwords);
+		break;
+	case 4:
+		cout << " ---- OPTION 4 ---- " << endl;
+		randomletters(vectorwords);
+		break;
+	case 5:
+		cout << " ---- OPTION 5 ---- " << endl;
+		cout << "Option unavailable" << endl;
+		break;
+	case 6:
+		cout << " End of program " << endl;
+		exit(1);
+		break;
+	default:
+		cout << "Option is invalid" << endl;
+	}
+}
+
+void usercontinue(vector <string> v1)
+{
+	char option;
+
+	cout << "Do you want to continue? Enter 'Y'es or 'N'o ";
+	cin >> option;
+
+	while (cin.fail() || option != 'Y' && option != 'y' && option != 'N' && option != 'n')
+	{
+		cin.clear();
+		cin.ignore(10000, '\n');
+		cout << "Invalid option " << endl;
+		cout << "Do you want to continue? Enter 'Y'es or 'N'o ";
+		cin >> option;
+	}
+
+	while (option == 'Y' || option == 'y')
+	{
+		menu(v1);
+		cout << "Do you want to continue playing? 'Y'es or 'N'o? ";
+		cin >> option;
+
+		while (cin.fail() || option != 'Y' && option != 'y' && option != 'N' && option != 'n')
 		{
-		case 1:
-			function1;
-			break;
-		case 2:
-			function2;
-			break;
-		case 3:
-			function3;
-			break;
-		case 4:
-			function4;
-			break;
-		case 5:
-			function5;
-			break;
-		default:
-			cout << "Option is invalid" << endl;
-                }
-
-
-	return 0;}
-
+			cin.clear();
+			cin.ignore(10000, '\n');
+			cout << "Invalid option " << endl;
+			cout << "Do you want to continue? Enter 'Y'es or 'N'o ";
+			cin >> option;
+		}
+	}
+}
